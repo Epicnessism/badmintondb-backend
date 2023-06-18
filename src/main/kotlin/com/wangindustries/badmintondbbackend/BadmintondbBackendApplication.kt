6bfb05dd4ca125ee6663e1901d34e866f5166b78
Tony Wang <tony.jy.wang@gmail.com>
2023-06-18 01:12:42 -0400

    added initial commit for spring project

---
+++ src/main/kotlin/com/wangindustries/badmintondbbackend/BadmintondbBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.wangindustries.badmintondbbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BadmintondbBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BadmintondbBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/wangindustries/badmintondbbackend/BadmintondbBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wangindustries.badmintondbbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BadmintondbBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
